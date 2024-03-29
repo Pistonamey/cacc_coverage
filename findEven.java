7c52197fddae45b763c84ef3249f0d71dab263d9
Amey Shinde <amey.shinde@mavs.uta.edu>
2024-03-28 23:51:59 -0500

    Update findEven.java

--- findEven.java
+++ findEven.java
@@ -8,7 +8,7 @@ public class findEven {
          * The integer does not have leading zeros.
          * The integer is even.
          * For example, if the given digits were [1, 2, 3], integers 132 and 312 follow the requirements.
-         * Return a sorted array of the unique integers.
+         * Return a sorted array of the unique integers
          */
         Set<Integer> result = new HashSet<>();
         for (int i = 0; i < digits.length - 2; i++) {
